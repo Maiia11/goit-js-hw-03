c4306ea212e2ff03c92e3d6fe5b532464986a129
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-02-16 23:01:37 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,28 @@
+function checkForSpam(message) {
+    const world1 = "spam";
+    const world2 = "sale";
+    if (message.toLowerCase().includes(world1.toLowerCase())) {
+        return true;
+    } else if (message.toLowerCase().includes(world2.toLowerCase())) {
+      return true;
+        } else {
+            return false;
+        }
+    }
+
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
+
+
+
+
+
+
+
